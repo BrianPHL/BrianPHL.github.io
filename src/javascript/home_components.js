b274639bf5983c5e84644c412df7e9a609ffabd7
Brian <brianpasco1206@gmail.com>
2022-03-24 08:58:06 +0800

    added: iconsCycle function on home component

---
+++ src/javascript/home_components.js
@@ -0,0 +1,13 @@
+const iconsComponent = $('.homeIcon');
+let iconsCount = 0;
+let iconsStart = 1;
+const iconsTable = [
+    '👋', '🌊', '👍',
+    '🤙', '💅'
+];
+function iconsCycle() {
+    let iconsIndex = (iconsStart + iconsCount) % iconsTable.length;
+    iconsCount++;
+    iconsComponent.html(iconsTable[iconsIndex]);
+}
+setInterval(iconsCycle, 2000);
