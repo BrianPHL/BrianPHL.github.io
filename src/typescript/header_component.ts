0ab4769a85d7b7dab98d7e1776c5709ab8248ad3
Brian <brianpasco1206@gmail.com>
2022-03-25 19:45:00 +0800

    finished: about me section in about component

--- src/typescript/header_component.ts
+++ src/typescript/header_component.ts
@@ -4,18 +4,16 @@ let lastScrollTop     = 0
 $(() => {
     
     
-    $( window ).on('scroll', function() {
+    $( window ).on('scroll', () => {
         
         let initialScrollTop = $(this).scrollTop();
         
         if ( initialScrollTop > lastScrollTop && lastScrollTop > 0 ) {
             
-            console.log('DOWN')
             headerComponent.css({'top': '-8vh'})
 
         } else {
             
-            console.log('UP')
             headerComponent.css({'top': '0vh'})
 
         }
