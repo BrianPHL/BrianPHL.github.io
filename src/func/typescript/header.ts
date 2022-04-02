09d9b92d748ebf36520c077e9d35258f88c2dbd4
Brian <brianpasco1206@gmail.com>
2022-04-02 20:19:04 +0800

    Added: onclick header animations

--- src/func/typescript/header.ts
+++ src/func/typescript/header.ts
@@ -1,19 +1,39 @@
-const navPanel = $('.header-navPanel')
-const reveal   = $('#reveal-navPanel')
-const conceal  = $('#conceal-navPanel')
+// * Mobile-view header
 
-$( reveal ).on('click', _ => {
-    console.log('1')
-    navPanel.animate({
-        'margin-right': '0%'
-    }, 300)
+const mvNavReveal  = document.querySelector('#mv-nav');
+const mvNavConceal = document.querySelector('#mv-nav-close');
+const mvNavLogo    = document.querySelector('#mv-logo')
+const mvNavPanel   = $('.header-mobile-nav');
+let   mvNavState   = false
+
+mvNavReveal.addEventListener('click', () => {
+
+    ( !mvNavState )
+    
+    ? (
+        mvNavPanel.animate({
+            'bottom': '0vh'
+        }, 100),
+        mvNavState = true 
+    )
+    : (
+        console.log('Error has occured in Ln.: 25-39')
+    )
 
 })
 
-$( conceal ).on('click', _ => {
-    console.log('2')
-    navPanel.animate({
-        'margin-right': '-100%'
-    }, 300)
+mvNavConceal.addEventListener('click', () => {
 
+    ( mvNavState ) 
+
+    ? (
+        mvNavPanel.animate({
+            'bottom': '-200px'
+        }, 100),
+        mvNavState = false
+    )
+    : (
+        console.log('Error has occured in Ln.: 41-52')
+    )
+    
 })
