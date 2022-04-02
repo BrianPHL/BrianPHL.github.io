64531a395d13125709cce5db67c267e93d2f982e
Brian <brianpasco1206@gmail.com>
2022-04-02 20:19:46 +0800

    Minified javascript commit

--- src/func/javascript/header.js
+++ src/func/javascript/header.js
@@ -1,15 +1,50 @@
-const navPanel = $('.header-navPanel');
-const reveal = $('#reveal-navPanel');
-const conceal = $('#conceal-navPanel');
-$(reveal).on('click', _ => {
-    console.log('1');
-    navPanel.animate({
-        'margin-right': '0%'
-    }, 300);
+// * Mobile-view header
+const mvNavReveal = document.querySelector('#mv-nav');
+const mvNavConceal = document.querySelector('#mv-nav-close');
+const mvNavLogo = document.querySelector('#mv-logo');
+const mvNavPanel = $('.header-mobile-nav');
+let mvNavState = false;
+mvNavReveal.addEventListener('click', () => {
+    (!mvNavState)
+        ? (mvNavPanel.animate({
+            'bottom': '0vh'
+        }, 100),
+            mvNavState = true)
+        : (console.log('Error has occured in Ln.: 25-39'));
 });
-$(conceal).on('click', _ => {
-    console.log('2');
-    navPanel.animate({
-        'margin-right': '-100%'
-    }, 300);
+mvNavConceal.addEventListener('click', () => {
+    (mvNavState)
+        ? (mvNavPanel.animate({
+            'bottom': '-200px'
+        }, 100),
+            mvNavState = false)
+        : (console.log('Error has occured in Ln.: 41-52'));
+});
+let themeState = "";
+const themePrefers = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const mvThemeToggle = document.querySelector('#mv-theme');
+const themeDark = () => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    mvThemeToggle.className = 'fi fi-rr-sun';
+    mvNavLogo.setAttribute("src", "/src/assets/dm-logo.png");
+};
+const themeLight = () => {
+    document.documentElement.setAttribute('data-theme', 'light');
+    mvThemeToggle.className = 'fa-solid fa-moon';
+    mvNavLogo.setAttribute("src", "/src/assets/lm-logo.png");
+};
+const checkPrefers = () => {
+    (themePrefers)
+        ? (themeDark(),
+            themeState = "dark")
+        : (themeLight(),
+            themeState = "light");
+};
+checkPrefers();
+mvThemeToggle.addEventListener('click', () => {
+    (themeState == 'light')
+        ? (themeDark(),
+            themeState = "dark")
+        : (themeLight(),
+            themeState = "light");
 });
