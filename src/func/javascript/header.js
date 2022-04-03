51c1e751924ec3b1b4dbd04f41693479830a450b
Brian <brianpasco1206@gmail.com>
2022-04-03 14:40:39 +0800

    Modified: switched header animation to VanillaJS

--- src/func/javascript/header.js
+++ src/func/javascript/header.js
@@ -2,21 +2,17 @@
 const mvNavReveal = document.querySelector('#mv-nav');
 const mvNavConceal = document.querySelector('#mv-nav-close');
 const mvNavLogo = document.querySelector('#mv-logo');
-const mvNavPanel = $('.header-mobile-nav');
+const mvNavPanel = document.querySelector('.header-mobile-nav');
 let mvNavState = false;
 mvNavReveal.addEventListener('click', () => {
     (!mvNavState)
-        ? (mvNavPanel.animate({
-            'bottom': '0vh'
-        }, 100),
+        ? (mvNavPanel.style.bottom = "0%",
             mvNavState = true)
         : (console.log('Error has occured in Ln.: 25-39'));
 });
 mvNavConceal.addEventListener('click', () => {
     (mvNavState)
-        ? (mvNavPanel.animate({
-            'bottom': '-40vh'
-        }, 100),
+        ? (mvNavPanel.style.bottom = "-22.5%",
             mvNavState = false)
         : (console.log('Error has occured in Ln.: 41-52'));
 });
