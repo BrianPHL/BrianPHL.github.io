fb0bc6859ffad09caf7624f1128b2aa455d51fa8
Brian <brianpasco1206@gmail.com>
2022-04-04 04:23:18 +0800

    Cleanup: removed comments and unused vars

--- src/func/typescript/init.ts
+++ src/func/typescript/init.ts
@@ -1,12 +1,9 @@
 var   themeState    = ""
 const themePrefers  = window.matchMedia("(prefers-color-scheme: dark)").matches;
-// const mvThemeToggle = document.querySelector('#mv-theme');
 
 const setThemeDark = () => {
 
     document.documentElement.setAttribute('data-theme', 'dark')
-    // mvThemeToggle.className = 'fi fi-rr-sun'
-    // mvNavLogo.setAttribute("src", "/src/assets/dm-logo.svg")
     localStorage.setItem("data-theme", "dark")
     themeState = "dark"
 
@@ -15,8 +12,6 @@ const setThemeDark = () => {
 const setThemeLight = () => {
 
     document.documentElement.setAttribute('data-theme', 'light')
-    // mvThemeToggle.className = 'fa-solid fa-moon'
-    // mvNavLogo.setAttribute("src", "/src/assets/lm-logo.svg")
     localStorage.setItem("data-theme", "light")
     themeState = "light"
 
